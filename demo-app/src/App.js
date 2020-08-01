9e9a9c63d13e12349b82777d5e735064bd815dd1
Angel Cochachez <acochachez@gmail.com>
2020-08-01 02:57:17 -0700

    Added serverless files

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -7,14 +7,14 @@ function App() {
 		<div className='App'>
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
-				<p>THIS IS A REACT APP FOR COMPLETE CODING</p>
+				<p>This is a react app for complete coding</p>
 				<a
 					className='App-link'
 					href='https://codifyme.github.io/AWSome-deploy-react/'
 					target='_blank'
 					rel='noopener noreferrer'
 				>
-					LEARN SERVELESS
+					Learn Serveless
 				</a>
 			</header>
 		</div>
