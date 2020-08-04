d510d10619840e039458a821f7f3d79fd1e6e6e7
Angel Cochachez <acochachez@gmail.com>
2020-08-04 13:52:36 -0700

    Added files of App.js, Index.js and package.json

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './awsome.png';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>This is a react app for complete coding</p>
-				<a
-					className='App-link'
-					href='https://codifyme.github.io/AWSome-deploy-react/'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn Serveless
-				</a>
-			</header>
-		</div>
-	);
+class App extends Component {
+  state = { 
+    isLoading:false,
+    invoices:[]
+  }
+
+  //setstate
+  render() { 
+
+      const isLoading=this.state.isLoading;
+      const invoices=this.state.invoices;
+      
+      const body=
+      [
+        {
+          "id":"100",
+          "Vendor":"Markdown",
+          "Amount":"$18,000",
+          "Date":"08/21/2019"
+        },
+        {
+          "id":"90",
+          "Vendor":"Markdown",
+          "Amount":"$28,000",
+          "Date":"08/21/2019"
+        },
+        {
+          "id":"80",
+          "Vendor":"Markdown",
+          "Amount":"$38,000",
+          "Date":"08/21/2019"
+        }
+      ]
+
+      if(isLoading)
+        return(<div>Loading...</div>);
+
+      return(
+        <div className="container border border-secondary rouded center">
+          <div className="row">
+            <div className="col-12">
+              <h4>Pending Invoices - The Test Company</h4>
+            </div>
+
+          </div>
+
+
+        </div>
+      );
+  }
 }
-
+ 
 export default App;
+
+// import React from 'react';
+// import logo from './awsome.png';
+// import './App.css';
+
+// function App() {
+// 	return (
+// 		<div className='App'>
+// 			<header className='App-header'>
+// 				<img src={logo} className='App-logo' alt='logo' />
+// 				<p>This is a react app for complete coding</p>
+// 				<a
+// 					className='App-link'
+// 					href='https://codifyme.github.io/AWSome-deploy-react/'
+// 					target='_blank'
+// 					rel='noopener noreferrer'
+// 				>
+// 					Learn Serveless
+// 				</a>
+// 			</header>
+// 		</div>
+// 	);
+// }
+
+// export default App;
